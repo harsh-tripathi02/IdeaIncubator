1084233f8db7344efcd777a45c342d6b65282cc5
harsh-tripathi02 <harsh.csai@gmail.com>
2025-04-16 13:27:31 +0530

    added docker file for frontend and backend

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const cors = require("cors")
 const ideaRoutes = require("./routes/ideaRoutes")
 const userRoutes = require("./routes/userRoutes")
 
+// Load environment variables from .env file
 dotenv.config()
 
 const app = express()
@@ -20,8 +21,13 @@ app.use("/api/ideas", ideaRoutes)
 app.use("/api/users", userRoutes)
 
 // ✅ Connect DB and Start Server
+const mongoURI = process.env.MONGO_URI;
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB")
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
